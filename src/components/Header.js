0ca7b312fac25ec69191ffaeaf2fa4df3a417406
ARJUN <princethakur932541@gmail.com>
2021-07-27 18:43:07 +0530

    complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,45 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
 import {Navbar,Nav,Container} from "react-bootstrap"
+import Slider from "@material-ui/core/Slider";
+
 
 const Header = (props) => {
+
+    
+    const [speed,setSpeed] = useState(3);
+    
+    function refreshPage() {
+        window.location.reload(false);
+      }
+
+    useEffect(() => {
+       props.setTime(500/speed);
+    }, [speed])
+   
+
     return (
         <>
         <Navbar expand="lg" bg="dark" variant="dark" style={{height:"10%"}}>
             <Container >
-            <button type="button" className="btn btn-primary btn-lg mr-2" onClick={() => props. genRandomArray()}>Generate Random Array</button>
-            <button type="button" className="btn btn-warning mr-1" onClick={() => props. setTime(1000)}>X/4</button>
-            <button type="button" className="btn btn-warning mr-1" onClick={() => props. setTime(400)}>X/2</button>
-            <button type="button" className="btn btn-warning mr-1" onClick={() => props. setTime(200)}>X</button>
-            <button type="button" className="btn btn-warning mr-1" onClick={() => props. setTime(100)}>2X</button>
-            <button type="button" className="btn btn-warning mr-1" onClick={() => props. setTime(50)}>4X</button>
+            <button type="button" className="btn btn-primary btn-lg mr-2"   onClick={() => refreshPage()}><i className="fas fa-redo"></i></button>
+            <button type="button" className="btn btn-primary btn-lg mr-2" disabled={props.loading ? true : false} onClick={() => props. genRandomArray()}>Generate</button>
+            <Slider
+                key={`slider-${speed}`}
+                defaultValue={speed}
+                onChange={(event, value) => setSpeed(value)}
+                aria-labelledby="discrete-slider"
+                step={1}
+                marks
+                min={1}
+                max={5}
+                style={{ flexGrow: 1, flexBasis: "100%" ,marginRight: "2%",marginLeft: "2%",color: "white"}}
+            />
             <Nav className="me-auto" >
-            <button type="button" className="btn btn-danger btn-lg  mr-3" onClick={() => props. bubbleSort()}>Bubble</button>
-            <button type="button" className="btn btn-danger btn-lg mr-3"  onClick={() => props. insertionSort()}>Insertion</button>
-            <button type="button" className="btn btn-danger btn-lg mr-3">Merge</button>
-            <button type="button" className="btn btn-danger btn-lg mr-3">Selection</button>
-            <button type="button" className="btn btn-danger btn-lg">Quick</button>
+            <button type="button" className="btn btn-danger btn-lg  mr-3" disabled={props.loading ? true : false} onClick={() => props. bubbleSort()}>Bubble</button>
+            <button type="button" className="btn btn-danger btn-lg mr-3" disabled={props.loading ? true : false} onClick={() => props. insertionSort()}>Insertion</button>
+            <button type="button" className="btn btn-danger btn-lg mr-3" disabled={props.loading ? true : false}  onClick={() => props. mergeSort()}>Merge</button>
+            <button type="button" className="btn btn-danger btn-lg mr-3" disabled={props.loading ? true : false}  onClick={() => props. selectionSort()}>Selection</button>
+            <button type="button" className="btn btn-danger btn-lg" disabled={props.loading ? true : false} onClick={() => props. quickSort()}>Quick</button>
             </Nav>
             </Container>
         </Navbar>
